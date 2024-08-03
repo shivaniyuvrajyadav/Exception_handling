32456fd3a0f5e6c0f9f1847df9b25e8d3d19fad0
Admin <Admin@LAPTOP-V3GL5398>
2024-08-03 22:18:32 +0530

    Add project files.

---
+++ Exception_handling/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exception_handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int i = 0;
+                int j = 0;
+
+                int k = i / j;
+
+            }
+            catch (Exception ex){
+            
+            Console.WriteLine(ex.Message);
+            
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+
+        }
+    }
+}
