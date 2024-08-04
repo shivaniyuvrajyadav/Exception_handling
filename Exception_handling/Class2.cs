467e487ee747a152f67df384a339d49b44f11287
Admin <Admin@LAPTOP-V3GL5398>
2024-08-04 17:29:44 +0530

    .

---
+++ Exception_handling/Class2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Exception_handling
+{
+    internal class Class2
+    { 
+        static void Main(string[] args)
+        {
+            string connectionstring = "hey";
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionstring)) ;
+
+                con.Open();
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
+
+/*
+     When to use Finally in real application ?
+
+      finally block  is mostly used to dispose the unwanted objects when they 
+      are no more required. this is good for performance , otherwise you have to wait 
+      for garbage collector to dispose them. 
+ */
