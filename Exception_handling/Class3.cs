fe7158e5bab026af70a478b8930474a098fc481b
Admin <Admin@LAPTOP-V3GL5398>
2024-08-04 17:45:38 +0530

    .

---
+++ Exception_handling/Class3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_handling
+{
+    internal class Class3
+    { 
+        static void Main(string[] args)
+        {
+            try
+            {
+                int i = 1;
+                int j = 0;
+                int k = i / j;
+               
+            }
+            finally
+            {
+                Console.WriteLine("try without catch block");
+
+            }
+
+        }
+
+    }
+}
+/*
+  can we have only try block without catch block in real application ?
+
+YES -  we can have only try block without catch block , but then we must have finnally block with try block.  
+ * 
+ */
