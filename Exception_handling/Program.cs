c33208ab7b662654dd4aa0f71993753409e24abe
Admin <Admin@LAPTOP-V3GL5398>
2024-08-04 14:59:11 +0530

    .

--- Exception_handling/Program.cs
+++ Exception_handling/Program.cs
@@ -25,3 +25,14 @@
         }
     }
 }
+/* 
+ How to implement Exception Handling in C#?
+Exception handling in Object- Oriented Programming is used to Manage Errors.
+
+1. try - a try block is a block of code inside which any error can occur . 
+
+2. catch - when any error occur in try block then it is passed to catch block to handle it . 
+
+3 . finally - the finally block is used to execute a given set of statements , whether an exception occur or not . 
+
+ */
