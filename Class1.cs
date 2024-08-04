2ed1e6677c28c7d3d1e298fadb07a2a0f6fc3119
Admin <Admin@LAPTOP-V3GL5398>
2024-08-04 17:05:14 +0530

    .

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{ 
+		static void Main(string[] args)
+		{
+			try
+			{
+				int i = 0;
+				int j = 0;
+
+				int k = i / j;
+			}
+			catch (ArithmaticException ex)
+			{
+				Console.WriteLine("Alert");
+				Console.WriteLine(ex.Message);
+			}
+			catch(ArgumentOutOfRangeException ex)
+			{
+				Console.WriteLine(ex.Message);
+				Console.WriteLine(ex.Message);
+			}
+		}
+	}
+}
---
+++ Exception_handling/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_handling
+{
+    internal class Class1
+    {  
+        static void Main(string[] args)
+        {
+            try
+            {
+                int i = 10;
+                int j = 0;
+                int k = i / j;
+            }
+            catch (ArithmeticException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            { 
+                Console.WriteLine("Exception");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
+/*
+ can we execute multiple catch blocks?
+
+  NO 
+
+we can write multiple catch blocks but when we will run the application and if 
+any error occur , only one out of them will execute based on the type of the error. 
+ */
